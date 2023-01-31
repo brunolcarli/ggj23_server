b417f550d4c32c1d01bc067de4267753f7c8123f
Bruno L. Carli <brunolcarli@gmail.com>
2023-01-30 22:34:40 -0300

    ADD character base classes

---
+++ server_app/character_classes.py
@@ -0,0 +1,8 @@
+
+
+classes = {
+    'dps': {'hp': 0, 'sp': 50, 'power': 5, 'resistance': 0, 'agility': 0},
+    'supporter': {'hp': 0, 'sp': 100, 'power': 0, 'resistance': 0, 'agility': 2},
+    'tanker': {'hp': 120, 'sp': 0, 'power': 0, 'resistance': 5, 'agility': 0},
+}
+
