992b7784bc7c77f6afddf8829441f0bdd4f308b0
Henrique Rieger <henriquerieger2001@gmail.com>
2023-02-04 09:12:48 -0300

    CHANGE version to 0.0.4

--- ggj23/settings.py
+++ ggj23/settings.py
@@ -17,7 +17,7 @@ import sys
 import uuid
 
 
-VERSION = '0.0.2'
+VERSION = '0.0.4'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
