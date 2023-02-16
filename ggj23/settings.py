15610d2306855f97bc7f2966f38b361dce6eb58e
Bruno L. Carli <brunolcarli@gmail.com>
2023-02-16 00:44:02 -0300

    CHANGE version

--- ggj23/settings.py
+++ ggj23/settings.py
@@ -17,7 +17,7 @@ import sys
 import uuid
 
 
-VERSION = '0.0.33'
+VERSION = '0.0.34'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
